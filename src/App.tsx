1d4cd53dbde67234810641519217b64042b0100b
Chi Nguyen <chi.nguyen@live.com>
2023-05-08 02:32:20 +0300

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <QueryClientProvider client={queryClient}>
         <Header />
         <Routes>
